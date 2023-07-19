be12d79e8ef36c68cd7e79c511014ee4f151098e
Rakesh Roshan M N <rakeshroshan@gmail.com>
2023-07-19 14:58:39 +0530

    code

---
+++ youtube-clone/src/components/Sidebar.jsx
@@ -0,0 +1,37 @@
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const SideBar = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        onClick={() => setSelectedCategory(category.name)}
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "#000",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "#000" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span>{category.name}</span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default SideBar;
